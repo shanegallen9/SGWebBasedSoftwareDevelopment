951a3def3436f0b0ee516221ba1855aff3c5d0ea
Shane Gallen <shanegallen9@gmail.com>
2017-07-06 15:46:53 +0100

    Update move.js

--- move/move.js
+++ move/move.js
@@ -2,11 +2,19 @@ var main = function() {
   
   var cities = [
   	"Zagreb",
+    "Bath",
+    "Belfast",
+    "Bristol",
+    "Edinburgh",
+    "Manchester",
+    "Madrid",
     "Paris",
     "London",
     "New York",
     "Tokio"
   ];
+  
+  cities.sort();
   
   $('#chooseYourCity').autocomplete({
 
